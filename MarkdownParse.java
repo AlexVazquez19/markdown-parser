71e221b88365a8ffe4ab4f0fb2368d3358a6f507
AlexVazquez19 <86645097+AlexVazquez19@users.noreply.github.com>
2022-04-14 17:19:54 -0700

    Image Links fix
    Updated MarkDownParse.java so that the getLinks() function no longer adds image links to the returned array.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,13 +13,16 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            int exclamationPoint = markdown.indexOf("!",currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if (exclamationPoint != openBracket - 1) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            currentIndex = closeParen + 1;   
         }
-
+        
         return toReturn;
     }
 
--- new_markdown.md
+++ new_markdown.md
@@ -1,3 +1,6 @@
-This is new text that I added in VSCode
+[hello there](this is not a link)
 
-[ this causes incorrect output ] ( not a link )
+
+[link1](https://something.com)
+[link2](some-thing.html)
+![Image](some-thing.png)
