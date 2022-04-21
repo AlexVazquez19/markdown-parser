6192f9ca6bee1bfeb866eac7ecc9f570b455c1f1
AlexVazquez19 <86645097+AlexVazquez19@users.noreply.github.com>
2022-04-21 16:43:41 -0700

    Create MarkdownParseTest.java

---
+++ MarkdownParseTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*; //imports the assert library from JUnit
+import org.junit.*; //imports the JUnit library as a whole
+
+public class MarkdownParseTest {
+    
+    @Test //tells JUnit that the following method is a unit test
+    public void Addition() { //test method
+        //Asserts that the first and second arguments should be equal
+        assertEquals(2, 1 + 1);  
+    }
+
+}
