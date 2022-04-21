134c31b44f1a36e7f44146878a8a94fa774de0e4
AlexVazquez19 <86645097+AlexVazquez19@users.noreply.github.com>
2022-04-21 16:46:08 -0700

    Update MarkdownParseTest.java
    compile: javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    
    run: java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,12 +1,11 @@
 import static org.junit.Assert.*; //imports the assert library from JUnit
 import org.junit.*; //imports the JUnit library as a whole
 
-public class MarkdownParseTest {
+public class MarkdownParseTest { //class declaration
     
     @Test //tells JUnit that the following method is a unit test
     public void Addition() { //test method
         //Asserts that the first and second arguments should be equal
         assertEquals(2, 1 + 1);  
     }
-
 }
